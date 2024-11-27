6901c466b609cab86459ef7b45dc20c239f64cb2
Killer-Cherry <miordonezh@unal.edu.co>
2024-11-27 16:53:58 -0500

    version 9

--- Ejercicios_trioMLL.py
+++ Ejercicios_trioMLL.py
@@ -2,29 +2,31 @@
 #@Laura Daniela Marín Barragan
 #@Miguel Andres Ordoñez Hernandez
 
-print("Programa para:\nEncontrar los litros de leche producidos en una semana en la granja, teniendo en cuenta que:\nPor 100m pastados se producen 15L de leche")
 ##Definir las variables ,y pedir los datos mediante un input para poder trabajar con los datos
 ##anteriormente transformados en en int
+print("\nPRODUCCIÓN DE LITROS DE LECHE CORTE SEMANAL.\n")
+
 LitrosxVaca= 15
 Dias=7
 Vacas = int(input("Digite la cantidad de vacas: "))
 def Litros (x,y,z):
      
     z=(x*z)//1
-    lol=(y*z)//1
-    return(lol)
+    cantidad=(y*z)//1
+    return(cantidad)
 print("Se produjeron",Litros(Vacas,Dias,LitrosxVaca),"litros","de leche con ",Vacas," vacas,en una semana de 7 días")
 ##PROBLEMAS
 def huevos():
-    aves=int(input("Ingrese el numero total de aves que hay en la granja. "))
+    print("\nPRODUCCIÓN DE HUEVOS CORTE MENSUAL.\n")
+    aves=int(input("Ingrese el numero total de aves que hay en la granja: "))
 
     gallinas=aves//3
     ponedoras=gallinas//2
-
+    print("\n\tUn tercio de las aves son gallinas ponedoras. ")
+    print("\tLa mitad de estas gallinas produce un huevo cada 3 dias y la otra otra mitad 1 cada 5 dias. ")
     p3=ponedoras*10
     p5=ponedoras*6
-
-    print("La gallinas ponen un total de ",p3+p5," huevos en un mes de 30 dias")
+    print("\nLa gallinas ponen un total de ",p3+p5," huevos en un mes de 30 dias\n")
 
 huevos()
-print("Prueba de carga")
+
