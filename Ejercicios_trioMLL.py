5307c588e4bd7b028062225bb4ea05ee37a35473
Laura Juliana <laurodriguezra@unal.edu.co>
2024-11-27 15:25:10 -0500

    Second commit

--- Ejercicios_trioMLL.py
+++ Ejercicios_trioMLL.py
@@ -1,1 +1,3 @@
-print("Trabajo grupal")
+print("Programa para encontrar los litros de leche producido en una semana en la granja: ")
+##Definir las variables ,y pedir los datos mediante un input para poder trabajar con los datos
+##anteriormente transformados en en int 
