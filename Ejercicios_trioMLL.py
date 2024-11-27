4051207fa90e2b71931569b2b7ed24d574c830db
Killer-Cherry428 <miordonezh@unal.edu.co>
2024-11-27 15:36:50 -0500

    Add files via upload

--- Ejercicios_trioMLL.py
+++ Ejercicios_trioMLL.py
@@ -1,3 +1,12 @@
 print("Programa para encontrar los litros de leche producido en una semana en la granja: ")
 ##Definir las variables ,y pedir los datos mediante un input para poder trabajar con los datos
 ##anteriormente transformados en en int 
+
+
+
+#Problema huevos
+aves=int(input("Ingrese el numero total de aves que hay en la granja. "))
+
+gallinas=aves/3
+ponedoras3=gallinas/2
+ponedoras5=gallinas
