eeade3ea7f209ea71d414fa69f9caba96ffe155d
Laura Juliana <laurodriguezra@unal.edu.co>
2024-11-27 16:08:53 -0500

    Five commit

--- Ejercicios_trioMLL.py
+++ Ejercicios_trioMLL.py
@@ -1,11 +1,16 @@
-print("Programa para encontrar los litros de leche producido en una semana en la granja: ")
+print("Programa para:\nEncontrar los litros de leche producidos en una semana en la granja, teniendo en cuenta que:\nPor 100m pastados se producen 15L de leche")
 ##Definir las variables ,y pedir los datos mediante un input para poder trabajar con los datos
 ##anteriormente transformados en en int
-
-
-
-#Problema huevos
-
+LitrosxVaca= 15
+Dias=7
+Vacas = int(input("Digite la cantidad de vacas: "))
+def Litros (x,y,z):
+     
+    z=(x*z)//1
+    lol=(y*z)//1
+    return(lol)
+print("Se produjeron",Litros(Vacas,Dias,LitrosxVaca),"litros","de leche con ",Vacas," vacas,en una semana de 7 días")
+##PROBLEMAS
 def huevos():
     aves=int(input("Ingrese el numero total de aves que hay en la granja. "))
 
@@ -18,20 +23,3 @@ def huevos():
     print("La gallinas ponen un total de ",p3+p5," huevos en un mes de 30 dias")
 
 huevos()
-print("Programa para:\nEncontrar los litros de leche producidos en una semana en la granja, teniendo en cuenta que:\nPor 100m pastados se producen 15L de leche")
-##Definir las variables ,y pedir los datos mediante un input para poder trabajar con los datos
-##anteriormente transformados en en int
-LitrosxVaca= 15
-Dias=7
-def Litros (Vacas,Dias,LitrosxVaca):
-    LitrosxVaca=(Vacas*LitrosxVaca)//1
-    x=(Dias*LitrosxVaca)//1
-    return(x)
-##anteriormente transformados en en int 
-def litros_de_leche():
-    vacas = int(input("Digite la cantidad de vacas: "))
-    leche = 15
-    cantidad = (7*leche)
-    litros = vacas * cantidad
-    print("Se produjeron ",litros," de leche con ",vacas," vacas")
-litros_de_leche()
