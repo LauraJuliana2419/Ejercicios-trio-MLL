d21a7626fba0c2d9fcca7d06e219aaadaaf88461
Laura Juliana <laurodriguezra@unal.edu.co>
2024-11-27 15:33:59 -0500

    Third commit

--- Ejercicios_trioMLL.py
+++ Ejercicios_trioMLL.py
@@ -1,3 +1,3 @@
-print("Programa para encontrar los litros de leche producido en una semana en la granja: ")
+print("Programa para:\nEncontrar los litros de leche producidos en una semana en la granja, teniendo en cuenta que:\nPor 100m pastados se producen 15L de leche")
 ##Definir las variables ,y pedir los datos mediante un input para poder trabajar con los datos
-##anteriormente transformados en en int 
+##anteriormente transformados en en int
